6e399e9add9763250ce46957821fb8c2f0d6b6f7
Collins Otieno <otienocollins0549@gmail.com>
2025-10-13 16:43:08 +0300

    updates

--- edulink/app/Http/Controllers/PaymentController.php
+++ edulink/app/Http/Controllers/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Payment;
+use App\Models\PaymentNotification;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use App\Services\NotificationService;
